fdba2c76817d8739ac067e32445372076250eaf9
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2018-11-27 10:18:24 -0800

    Writing to queue, need to now process from queue

--- functions/pipeline/onboardcontainer/__init__.py
+++ functions/pipeline/onboardcontainer/__init__.py
@@ -4,6 +4,7 @@ import json
 import azure.functions as func
 
 from azure.storage.blob import BlockBlobService
+from azure.storage.queue import QueueService
 DEFAULT_RETURN_HEADER= { "content-type": "application/json"}
 
 
@@ -27,11 +28,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         storage_account = req_body["storageAccount"]
         storage_account_key = req_body["storageAccountKey"]
         storage_container = req_body["storageContainer"]
+        onboard_queue = req_body["onboardQueue"]
     except ValueError:
         return func.HttpResponse("ERROR: Unable to decode POST body", status_code=400)
 
-    if not storage_container or not storage_account or not storage_account_key:
-        return func.HttpResponse("ERROR: storage container/account/key not specified.", status_code=401)
+    if not storage_container or not storage_account or not storage_account_key or not onboard_queue:
+        return func.HttpResponse("ERROR: storage container/account/key/queue not specified.", status_code=401)
 
     # TODO: Get list of all items inside storage_container
     # TODO: limit by file types/postfix?
@@ -42,10 +44,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     blob_service = BlockBlobService(account_name=storage_account,
                                     account_key=storage_account_key)
 
+    queue_service = QueueService(account_name=storage_account, account_key=storage_account_key)
+
     try:
         blob_list = []
         for blob_object in blob_service.list_blobs(storage_container):
             blob_list.append(blob_service.make_blob_url(storage_container, blob_object.name))
+
+        for image_url in blob_list:
+            queue_service.put_message(onboard_queue, image_url)
+
         return func.HttpResponse(
             status_code=200,
             headers=DEFAULT_RETURN_HEADER,
